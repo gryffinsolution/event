3ebff3bd907fa8c05c1119977f03e3bfb073dae4
gryffinsolution <gryffinsolution@gmail.com>
2018-09-18 22:29:43 +0900

    lowercase

--- src/util/ASao.java
+++ src/util/ASao.java
@@ -14,12 +14,8 @@ import org.apache.logging.log4j.Logger;
 public class ASao {
 	private static final Logger LOG = LogManager.getLogger(ASao.class);
 
-	public boolean isWorking() {
-		return true;
-	}
-
 	public String getEventGet( String host,int port){
-		String url ="http://"+host+":"+port+"/getEvent";
+		String url ="http://"+host+":"+port+"/getevent";
 		HttpURLConnection con = null;
 		try{
             URL myurl = new URL(url);
@@ -52,46 +48,6 @@ public class ASao {
         }catch (Exception e){
         	LOG.error(e);
         	return null;
-        }finally {
-            con.disconnect();
-		}
-	}
-	
-	public boolean setEventEndTimestamp(String host, int port,long id) {
-		String url ="http://"+host+":"+port+"/setLastEventId?eventID="+id;
-		HttpURLConnection con = null;
-		try{
-            URL myurl = new URL(url);
-            con = (HttpURLConnection) myurl.openConnection();
-            con.setRequestMethod("GET");
-            con.setReadTimeout(100);//TEST
-            StringBuilder content;
-            try (BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()))) {
-
-                String line;
-                content = new StringBuilder();
-
-                while ((line = in.readLine()) != null) {
-                    content.append(line);
-                    content.append(System.lineSeparator());
-                }
-            }
-            System.out.println(content.toString());
-            con.disconnect();
-            return true;
-        }catch (MalformedURLException e){
-        	LOG.error(e);
-        	return false;
-        }catch (ProtocolException e){
-        	LOG.error(e);
-        	return false;
-        }catch (IOException e){
-        	LOG.error(e);
-        	return false;
-        }catch (Exception e){
-        	LOG.error(e);
-        	return false;
         }finally {
             con.disconnect();
 		}
